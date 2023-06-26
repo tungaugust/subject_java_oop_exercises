e50dde7962209145a510398718f4f8a267b05645
tungaugust <tungpt96august@gmail.com>
2023-06-26 15:08:26 +0700

    Complete from 4 Exercise to Exercise 14 of Unit 3 in Module 1.

--- src/main/module_one/App.java
+++ src/main/module_one/App.java
@@ -1,5 +1,7 @@
 package main.module_one;
 
+import java.util.PrimitiveIterator;
+
 public class App {
     public static void run(String[] args) {
         System.out.println("***** Module 1 *****");
@@ -30,54 +32,70 @@ public class App {
 
         /* ===== */
         System.out.println("\n*** Exercise 4. GIAI PHUONG TRINH BAC MOT: Ax + B = 0.");
+        ProgramsOfModuleOne.linearEquationOneVariableSolution(0.0, 0.0);
+        ProgramsOfModuleOne.linearEquationOneVariableSolution(0, 1);
+        ProgramsOfModuleOne.linearEquationOneVariableSolution(2, 1);
 
         /* ===== */
-        System.out.println("\n*** Exercise 5. TIM UOC SO CHUNG LON NHAT CUA HAI SO.");
+        System.out.println("\n*** Exercise 5. TIM UOC SO CHUNG LON NHAT CUA HAI SO (Greatest Common Divisor).");
+        ProgramsOfModuleOne.greatestCommonDivisorWithSubtraction(150, 60);
+        ProgramsOfModuleOne.greatestCommonDivisorWithRecursive(150, 60);
 
         /* ===== */
         System.out.println("\n*** Exercise 6. KIEM TRA SO NGUYEN TO.");
+        ProgramsOfModuleOne.primeNumber(173);
+        ProgramsOfModuleOne.primeNumber(114);
 
         /* ===== */
         System.out.println("\n*** Exercise 7. TINH TONG CAC SO NGUYEN TO NHO HON SO N.");
+        ProgramsOfModuleOne.sumOfPrimeNumberLessThan(10);
 
         /* ===== */
         System.out.println("\n*** Exercise 8. TINH TONG N SO NGUYEN TO DAU TIEN.");
+        ProgramsOfModuleOne.sumOfFirstNNumbersPrimeNumber(5);
 
         /* ===== */
         System.out.println("\n*** Exercise 9. TIM SO LAN KY TU (CHARACTER) XUAT HIEN TRONG MOT CHUOI (STRING).");
+        ProgramsOfModuleOne.countCharacterInString('l',"hello");
 
         /* ===== */
         System.out.println("\n*** Exercise 10. TACH CHUOI GOC THANH CAC CHUOI CON.");
+        ProgramsOfModuleOne.splitStringIntoWords("TACH          CHUOI          GOC       THANH         CAC CHUOI CON");
 
         /* ===== */
-        System.out.println("\n*** Exercise 11. DEM KY TU LA CHU SO (DIGITAL) CO TRONG MOT CHUOI. SU DUNG BANG MA ASCII.");
-
+        System.out.println("\n*** Exercise 11. DEM KY TU LA CHU SO (DIGITS) CO TRONG MOT CHUOI. SU DUNG BANG MA ASCII.");
+        ProgramsOfModuleOne.countDigitsInString("Nam hai ngan 0 tram 2 muoi 3.");
         /* ===== */
         System.out.println("\n*** Exercise 12. TINH TONG 10 SO CHAN DAU TIEN.");
+        ProgramsOfModuleOne.sumFirstNEvenNumbers(10);
 
         /* ===== */
         System.out.println("\n*** Exercise 13. IN RA SO LE TU A DEN B. VI DU: TU 1 DEN 99.");
+        ProgramsOfModuleOne.printOddNumbers(1,33);
+        ProgramsOfModuleOne.printOddNumbers(2,30);
 
         /* ===== */
         System.out.println("\n*** Exercise 14. TINH TONG CAC SO LA BOI SO CUA 7 TRONG PHAM VI TU 1 DEN 100.");
+        ProgramsOfModuleOne.printMultipleFromTo(7, 1, 100);
 
         /* ===== */
-        System.out.println("\n*** Exercise 15. TINH TONG CAC SO LA BOI SO CUA 7 TRONG PHAM VI TU 1 DEN 100.");
+        System.out.println("\n*** Exercise 15. TIM GIA TRI LON NHAT VA NHO NHAT TRONG DAY SO.");
+//        ProgramsOfModuleOne.findMinMaxFromInput();
 
-        /* ===== */
-        System.out.println("\n*** Exercise 16. IN RA MOT GIA TRI SO NGUYEN, XAC DINH SO DO LA SO CHAN, SO LE HAY ZERO.");
-
-        /* ===== */
-        System.out.println("\n*** Exercise 17. TINH TONG CAC SO LA BOI SO CUA 3 TRONG PHAM VI TU 300 DEN 3.");
-
-        /* ===== */
-        System.out.println("\n*** Exercise 18. IN RA N SO DAU TIEN CUA DAY SO FINBONACCI VOI HAI GIA TRI DAU TIEN LA 1 VA 1.");
-
-        /* ===== */
-        System.out.println("\n*** Exercise 19. IN RA CAC HINH (DUNG CAU TRUC LAP).");
-
-        /* ===== */
-        System.out.println("\n*** Exercise 20. NHAP HAI SO NGUYEN DUONG M VA N, IN RA CAC HINH (DUNG CAU TRUC LAP).");
+//        /* ===== */
+//        System.out.println("\n*** Exercise 16. IN RA MOT GIA TRI SO NGUYEN, XAC DINH SO DO LA SO CHAN, SO LE HAY ZERO.");
+//
+//        /* ===== */
+//        System.out.println("\n*** Exercise 17. TINH TONG CAC SO LA BOI SO CUA 3 TRONG PHAM VI TU 300 DEN 3.");
+//
+//        /* ===== */
+//        System.out.println("\n*** Exercise 18. IN RA N SO DAU TIEN CUA DAY SO FINBONACCI VOI HAI GIA TRI DAU TIEN LA 1 VA 1.");
+//
+//        /* ===== */
+//        System.out.println("\n*** Exercise 19. IN RA CAC HINH (DUNG CAU TRUC LAP).");
+//
+//        /* ===== */
+//        System.out.println("\n*** Exercise 20. NHAP HAI SO NGUYEN DUONG M VA N, IN RA CAC HINH (DUNG CAU TRUC LAP).");
 
 
     }
--- src/main/module_one/ProgramsOfModuleOne.java
+++ src/main/module_one/ProgramsOfModuleOne.java
@@ -1,5 +1,8 @@
 package main.module_one;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ProgramsOfModuleOne {
 
     // Exercise 3
@@ -23,8 +26,189 @@ public class ProgramsOfModuleOne {
     }
     // Exercise 4
     public static void linearEquationOneVariableSolution(double a, double b){
-        // Ax + B = 0
-        double result; // x
+        // Form  of Linear Equation in One Variable: Ax + B = 0
+        // Case 1: A = 0, B = 0 => Phuong trinh vo so nghiem.
+        // Case 2: A = 0, B != 0 => Phuong trinh vo nghiem.
+        // Case 3: A != 0 => Phuong trinh co mot nghiem la x = -B/A.
+        if (a == 0) {
+            if (b == 0){
+                System.out.printf("Phuong trinh %.2fX + %.2f = 0 la phuong trinh vo so nghiem.\n", a, b);
+            } else {
+                System.out.printf("Phuong trinh %.2fX + %.2f = 0 la phuong trinh vo nghiem.\n", a, b);
+            }
+        } else {
+            System.out.printf("Phuong trinh %.2fX + %.2f = 0 co mot nghiem: X = %.2f\n", a, b, -b/a);
+        }
+    }
+    // Exercise 5: Dung giai thuat Giai thuat Euclid.
+    public static void greatestCommonDivisorWithDivision(int a, int b){
+//        if (a >= b)
+        return;
+    }
+    public static void greatestCommonDivisorWithSubtraction(int a, int b){
+        int x = a;
+        int y = b;
+        while(x!= y){
+            if (x > y){
+                x = x - y;
+            } else {
+                y = y - x;
+            }
+        }
+        System.out.printf("Uoc so chung lon nhat cua %d va %d la %d.\n", a, b, x);
+    }
+    private static int _greatestCommonDivisorWithRecursive(int a, int b){
+        if (b == 0){
+            return a;
+        } else {
+            return _greatestCommonDivisorWithRecursive(b, a % b);
+        }
+    }
+    public static void greatestCommonDivisorWithRecursive(int a, int b){
+        System.out.printf("Uoc so chung lon nhat cua %d va %d la %d.\n", a, b, _greatestCommonDivisorWithRecursive(a, b));
+    }
 
+    // Exercise 6
+    public static boolean isPrimeNumber(int n){
+        if (n < 2){
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++){
+            if (n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void primeNumber(int n){
+        if (isPrimeNumber(n)){
+            System.out.println("So " + n + " la so nguyen to.");
+        } else {
+            System.out.println("So " + n + " khong la so nguyen to.");
+        }
+    }
+    // Exercise 7
+    public static void sumOfPrimeNumberLessThan(int n){
+        int sum = 0;
+        for (int i = 2; i < n; i++){
+            if (isPrimeNumber(i)){
+                sum += i;
+            }
+        }
+        System.out.println("Tong cac so nguyen to nho hon so " + n + " la " + sum);
+    }
+    // Exercise 8
+    public static void sumOfFirstNNumbersPrimeNumber(int n){
+        int sum = 0;
+        int k = 0;
+        int i = 2;
+        while(k < n){
+            if (isPrimeNumber(i)){
+                k += 1;
+                sum += i;
+            }
+            i++;
+        }
+        System.out.println("Tong cua " + n + " so nguyen to dau tien la " + sum);
+    }
+    // Exercise 9
+    public static void countCharacterInString(char c, String s){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++){
+            if( s.charAt(i) == c){
+                count++;
+            }
+        }
+        System.out.println("So lan xuat hien cua ky tu '" + c + "' trong chuoi '" + s + "' la " + count + " lan.");
+    }
+    public static void splitStringIntoWords(String s){
+        int size = s.length();
+        String sub;
+        ArrayList<String> strArray = new ArrayList<String>();
+        for (int i = 0; i < size; i++){
+            if (s.charAt(i) != ' '){
+                // starting a word
+                sub = "";
+                for (int j = i; j < size; j++){
+                    i = j;
+                    if (s.charAt(j) != ' '){
+                        sub += s.charAt(j);
+                    } else {
+                        // ending a word
+                        break;
+                    }
+                }
+                strArray.add(sub);
+            }
+        }
+        for (String str: strArray){
+            System.out.println(str);
+        }
+    }
+    public static void countDigitsInString(String s){
+        int count = 0;
+        for (int i = 0; i < s.length(); i++){
+            if(Character.isDigit(s.charAt(i))){
+                count++;
+            }
+        }
+        System.out.println("So ky tu chu so co trong chuoi '" + s +  "' la " + count + " ky tu.");
+    }
+    public static void sumFirstNEvenNumbers(int n){
+        int sum  = 0;
+        int k = 0;
+        int i = 0;
+        while(k < n){
+            i += 2;
+            sum += i;
+            k++;
+        }
+        System.out.println("Tong cua " + n + " so chan dau tien (khong tinh so 0) la " + sum);
+    }
+    public static void printOddNumbers(int start, int end){
+        System.out.print(start + "->" + end + ": ");
+        for (int i = start; i < end; i++){
+            if (i %2 != 0){
+                System.out.print(i + " ");
+            }
+        }
+        if (end % 2 != 0){
+            System.out.println(end);
+        } else {
+            System.out.println();
+        }
+    }
+    public static void printMultipleFromTo(int num, int start, int end){
+        System.out.println("Boi so cua " + num + " trong pham vi tu " + start + " den  " + end + ":");
+        int multiple = num;
+        int k = 1;
+        while(multiple <= end) {
+            multiple = num * k;
+            if (multiple >= start && multiple <= end){
+                System.out.print(multiple + " ");
+            }
+            if (multiple > end){
+                break;
+            }
+            k++;
+        }
+        System.out.println();
+    }
+    public static void findMinMaxFromInput(){
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
+        System.out.println("Nhap day so nguyen tu ban phim: ");
+        while(sc.hasNextInt()){
+            int num = sc.nextInt();
+            numbers.add(num);
+        }
+        int min = numbers.get(0);
+        int max = numbers.get(1);
+        for(int i = 0; i < numbers.size() - 1; i++){
+            if (min > numbers.get(i + 1)){
+                min  = numbers.get(i + 1);
+            }
+        }
+        System.out.println(min);
     }
 }
