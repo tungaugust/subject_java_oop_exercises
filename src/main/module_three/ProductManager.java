4d56956f4dc0d8fa0e556a457739145e5dd2c28e
tungaugust <tungpt96august@gmail.com>
2023-07-04 23:23:49 +0700

    Doing Exercise 7 of Module 3.

--- src/main/module_three/ProductManager.java
+++ src/main/module_three/ProductManager.java
@@ -1,4 +1,52 @@
 package main.module_three;
 
 public class ProductManager {
+    private Product[] products;
+    private int capacity;
+    private int size;
+
+    public Product[] getProducts() {
+        return this.products;
+    }
+
+    private void setProducts(Product[] products) {
+        this.products = products;
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    private void setCapacity(int capacity) {
+        if (capacity < 0) {
+            capacity = 0;
+        }
+        this.capacity = capacity;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    private void setSize(int size) {
+        this.size = size;
+    }
+    private void increaseSize(){
+        setSize(getSize() + 1);
+    }
+
+    private void decreaseSize(){
+        setSize(getSize() - 1);
+    }
+
+    public ProductManager(int capacity) {
+        this.setCapacity(capacity);
+        this.setSize(0);
+        this.setProducts(new Product[this.getCapacity()]);
+    }
+
+    public boolean add(Product product){
+
+        return false;
+    }
 }
